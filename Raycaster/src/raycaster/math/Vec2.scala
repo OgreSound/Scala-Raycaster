c2d6501c003f9902d687cc542f1a45ddfb5a5ee8
Vuorjoki Aleksi <vuorjoa1@niksula.hut.fi>
2014-02-21 02:14:41 +0200

    implemented Vec2 methods *,+ and -, and created tests for them.

--- Raycaster/src/raycaster/math/Vec2.scala
+++ Raycaster/src/raycaster/math/Vec2.scala
@@ -3,9 +3,41 @@
  */
 package raycaster.math
 /**
- * @author OgreSound
+ * @author Vuorjoki Aleksi
  *
  */
-class Vec2 {
+class Vec2(val x: Float, val y: Float) {
 
+  /**
+   * Returns new vector that is this vector multiplied by the given coefficient
+   *
+   * @param the coefficient the vector should be multiplied by
+   * @return a new multiplied vector
+   */
+
+  def *(coefficient: Float): Vec2 = {
+    new Vec2(x * coefficient, y * coefficient)
+  }
+ 
+  /**
+   * Returns a new vector that is the sum of this vector and the given vector
+   *
+   * @param the vector that should be added to this vector
+   * @return the sum vector
+   */
+  
+  def +(vector: Vec2): Vec2 = {
+    new Vec2(this.x + vector.x , this.y + vector.y)
+  }
+
+  /**
+	 * Returns a new vector resulting from subtracting the given vector from this vector
+	 * 
+	 * @param the vector that should be subtracted from this vector
+	 * @return the subtracted vector
+	 */
+  
+  def -(vector: Vec2): Vec2 = {
+    new Vec2(this.x - vector.x , this.y - vector.y)
+  }
 }
---
+++ Raycaster/src/raycaster/tests/Vec2Test.scala
@@ -0,0 +1,50 @@
+package raycaster.tests
+
+import org.scalatest._
+import raycaster.math.Vec2
+class Vec2Test extends FlatSpec with Matchers {
+
+  "*" should "multiply vector with the given coefficient" in {
+    val vector = new Vec2(4, 3)
+    val coefficient: Float = 2
+    
+    assume(vector.x == 4)
+    assume(vector.y == 3)
+    
+    val product = vector * coefficient
+    
+    
+    assert(product.x == 8)
+    assert(product.y == 6)
+  }
+  
+  "+" should "add two vectors together" in {
+    val vector1 = new Vec2(4,3)
+    val vector2 = new Vec2(1,2)
+    
+    assume(vector1.x == 4)
+    assume(vector1.y == 3)
+    assume(vector2.x == 1)
+    assume(vector2.y == 2)
+    
+    val sum = vector1 + vector2
+    
+    assert(sum.x == 5)
+    assert(sum.y == 5)
+  }
+  
+  "-" should "should subtract vector2 from vector1" in {
+    val vector1 = new Vec2(4,3)
+    val vector2 = new Vec2(1,2)
+    
+    assume(vector1.x == 4)
+    assume(vector1.y == 3)
+    assume(vector2.x == 1)
+    assume(vector2.y == 2)
+    
+    val substraction = vector1 - vector2
+    
+    assert(substraction.x == 3)
+    assert(substraction.y == 1)
+  }
+}
