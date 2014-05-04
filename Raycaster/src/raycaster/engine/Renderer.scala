a7ed19532c5efe2c203403a031ebc2fb9ab7fbba
Vuorjoki Aleksi <vuorjoa1@niksula.hut.fi>
2014-05-05 00:24:37 +0300

    added a file chooser to load a map

--- Raycaster/src/raycaster/engine/Renderer.scala
+++ Raycaster/src/raycaster/engine/Renderer.scala
@@ -22,6 +22,8 @@ class Renderer(private var w: Int, private var h: Int, private var mapFile: File
   def dir = player.direction
   def plane = Vec2(-dir.y, dir.x)
 
+  def loadMap(mapFile:File) = map = new Map(mapFile)
+  
   def render(g: Graphics2D): BufferedImage = {
     val img = new BufferedImage(w, h, TYPE_INT_ARGB)
     val gr = img.createGraphics()
--- Raycaster/src/raycaster/ui/Raycaster.scala
+++ Raycaster/src/raycaster/ui/Raycaster.scala
@@ -5,8 +5,14 @@ package raycaster.ui
 
 import scala.swing.SimpleSwingApplication
 import scala.swing.MainFrame
+import scala.swing._
 import java.awt.Dimension
 import java.io.File
+import scala.swing.FileChooser
+import java.io.FileFilter
+import javax.swing.filechooser.FileFilter
+import javax.swing.filechooser.FileNameExtensionFilter
+
 
 /**
  * @author OgreSound
@@ -17,17 +23,30 @@ object Raycaster extends SimpleSwingApplication {
     private val defaultHeight = 720
     private var width = defaultWidth
 	private var height = defaultHeight
-	private var mapFile = new File("RayCasterMapTextured.world")
-	
+	private var mapFile = new File("maps/RayCasterMapTextured.world")
+    private val path = mapFile.getCanonicalPath()
+	val caster = new RaycasterPanel(width,height,mapFile)
   val mainWindow = new MainFrame {
 	  title = "Raycaster"
 	  minimumSize = new Dimension(width,height)
 	  centerOnScreen()
 	  resizable= false
-	  val caster = new RaycasterPanel(width,height,mapFile)
+	  
 	  contents = caster
+	  menuBar = new MenuBar {
+	    contents += new Menu("Game"){
+	      contents += new MenuItem(Action("load a map")(openMap))
+	    }
+	  }
 	}
 	
 	def top = mainWindow
+	def openMap = {
+	  val chooser = new FileChooser(new File(path))
+	  chooser.fileFilter = new FileNameExtensionFilter("game maps", "world")
+	  if(chooser.showOpenDialog(null) == FileChooser.Result.Approve){
+	    caster.renderer.loadMap(chooser.selectedFile)
+	  } 
+	}
 	
 }
